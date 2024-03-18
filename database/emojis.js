e684b08348c454b7ec42b7634baf47a5f62f3140
Munyoung Cho <munyoungc31@gmail.com>
2024-03-18 00:53:28 -0700

    Fix add emoji

--- database/emojis.js
+++ database/emojis.js
@@ -19,7 +19,7 @@ async function getEmojis() {
 
 async function addEmojiToChat(postData) {
   let addEmojiToChatSQL = `
-		INSERT INTO emoji_reactions (message_id, emoji_id, user_id)
+		INSERT INTO emojireactions (message_id, emoji_id, user_id)
     VALUES (:message_id, :emoji_id, :user_id);
 	`;
 
@@ -41,34 +41,7 @@ async function addEmojiToChat(postData) {
   }
 }
 
-async function getEmojiReactions(postData) {
-  let getEmojiReactionsSQL = `
-		SELECT emoji_reactions.message_id, emoji.emoji_id, emoji.image, COUNT(*) as count_emoji
-        FROM emoji_reactions
-        JOIN emoji ON emoji.emoji_id = emoji_reactions.emoji_id
-        WHERE message_id = :message_id and user_id = :user_id
-        GROUP BY message_id, emoji_id
-	`;
-
-  params = {
-    message_id: postData.message_id,
-    user_id: postData.user_id,
-  };
-
-  try {
-    const results = await database.query(getEmojiReactionsSQL, params);
-    console.log("Successfully found emoji reactions");
-    // console.log(results[0]);
-    return results[0];
-  } catch (err) {
-    console.log("Error trying to find emoji reactions");
-    console.log(err);
-    return false;
-  }
-}
-
 module.exports = {
   getEmojis,
   addEmojiToChat,
-  getEmojiReactions,
 };
